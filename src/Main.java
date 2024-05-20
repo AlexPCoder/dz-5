cdf3ee8b9e5608cba2a520c3d08460b50c9ad219
OleksiiPodluzhnyi <alexeypodluzhniy@gmail.com>
2024-05-20 10:57:36 +0200

    add solution

--- src/Main.java
+++ src/Main.java
@@ -2,11 +2,23 @@
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("");
+    int a = 1;
+    int b = 1;
+    int c = 2;
+    int d = 1;
 
+    int sum1 = a + b;
+    int sum2 = c + d;
 
-        }
+    System.out.println("Перше порівняння: " + (sum1 > sum2));
+
+    sum1 += 1;
+
+    sum2 -= 2;
+
+    System.out.println("Друге порівняння: " + (sum1 > sum2));
+
+        boolean isSumEven = (sum1 % 2 == 0) || (sum2 % 2 == 0);
+        System.out.println("Чи є хоча б одна сума кратною 2: " + isSumEven);
     }
-}
+    }
